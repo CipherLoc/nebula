9f771c8ac8b230ef2c0294fe33eb83f5222b8289
Jon Rafkind <jon@cipherloc.net>
2022-11-27 16:45:11 +0000

    use a set for ip data

--- hooks.go
+++ hooks.go
@@ -9,16 +9,26 @@ type Hook interface {
 	FirewallDrop(packet firewall.Packet)
 }
 
+// FIXME: need a better name
+type DropData struct {
+	IP iputil.VpnIp
+	Port uint16
+	Protocol uint8
+}
+
+/* track a single instance of (ip, port, protocol)
+ */
 type FirewallIncomingHook struct {
-	Drops map[iputil.VpnIp]int
+	Drops map[DropData]bool
 }
 
 func NewFirewallIncomingHook() *FirewallIncomingHook {
 	return &FirewallIncomingHook{
-		Drops: make(map[iputil.VpnIp]int),
+		Drops: make(map[DropData]bool),
 	}
 }
 
 func (hook *FirewallIncomingHook) FirewallDrop(packet firewall.Packet){
-	hook.Drops[packet.RemoteIP] = int(packet.RemotePort)
+	data := DropData{IP: packet.RemoteIP, Port: packet.LocalPort, Protocol: packet.Protocol}
+	hook.Drops[data] = true
 }
